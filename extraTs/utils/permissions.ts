160cf5f28a3f4cc75bcdcf4f2f5246acbc2e376b
Rohit0203 <56021080+Rohit0203@users.noreply.github.com>
2019-10-18 17:39:09 +0530

    Update permissions.ts

--- extraTs/utils/permissions.ts
+++ extraTs/utils/permissions.ts
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-import {permissions} from './constants';
-export  const hasPermissions=(moduleName: string,role: any,permissionType: string)=>
-{
-    if(Object.keys(permissions).includes(moduleName))
-    {
-        if(Object.keys(permissions)[role].includes(permissionType))
-        {
-            console.log('hello');
-            
-
-        }
-        else
-        {
-            return 'Permissions Type is not present'
-        }
-
-    }
-    else{
-        return 'Module is Not present';
-    }
-}
-
-=======
 import { permissions } from './constants';
->>>>>>> Day8-39522
-
 const { getUsers } = permissions;
 export const hasPermissions = (
   moduleName: string,
