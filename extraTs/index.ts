0a26f91be3cab7e5d4ad2f08fce889399040a1d6
rohit0203 <rohit.singh@successive.tech>
2019-10-18 15:15:23 +0530

    removed logging

--- extraTs/index.ts
+++ extraTs/index.ts
@@ -1,27 +1,19 @@
-import {hasPermissions,User} from './utils/index';
-import  {DrawDiamond,DrawEquilateral} from './patterns/index';
+import { hasPermissions, User } from './utils/index';
+import { DrawDiamond, DrawEquilateral } from './patterns/index';
 
-import {users} from './utils/constants';
+import { users } from './utils/constants';
 
-const dd= new DrawDiamond();
-const de=new DrawEquilateral();
-const user =new User();
+const dd = new DrawDiamond();
+const de = new DrawEquilateral();
+const user = new User();
 user.validateUsers(users);
-hasPermissions('getUsers','trainee','read')
-
-
-
-
+hasPermissions('getUsers', 'trainee', 'read');
 
 /* THIS IS FOR DRAWING PATTERN */
-let rows:number =parseInt(process.argv[2])
-if(rows<11 && rows>2)
-{
-    dd.draw(rows);
-    de.draw(rows);
-}
-else
-{
- console.error('Rows should be in between 2 to 10');
- 
+let rows: number = parseInt(process.argv[2]);
+if (rows < 11 && rows > 2) {
+  dd.draw(rows);
+  de.draw(rows);
+} else {
+  console.error('Rows should be in between 2 to 10');
 }
