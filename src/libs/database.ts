c1b53440a488ae43b36f5270c64809aceb9ad1be
Rohit0203 <rohit.singh@successive.tech>
2019-10-21 12:38:52 +0530

    Day 13 task

--- src/libs/database.ts
+++ src/libs/database.ts
@@ -1,10 +1,13 @@
 import * as mongoose from 'mongoose';
+import seedInitial from '../libs/seedData';
+
 export default class Database {
     public open(url: string) {
         return mongoose.connect(url, { useNewUrlParser: true }, err => {
             if (err) {
                 return err;
             }
+            seedInitial();
             return true;
         });
     }
---
+++ src/libs/seedData.ts
@@ -0,0 +1,5 @@
+
+const seddInitial = () => {
+
+}
+export default seddInitial;
